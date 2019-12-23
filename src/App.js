24a77bd8db3c3b2ff8e100c72976280b02976c54
HermersonDev <ne.hermerson@gmail.com>
2019-12-22 22:41:01 -0300

    Modification on the page App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import React from 'react';
 import Routes from './Routes';
 
 function App() {
-  return (
-    <div className="App">
-    	<Routes />
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
